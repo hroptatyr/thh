6505abbc2458729f42cf0e02d81436b8be7b4964
Sebastian Freundt <freundt@ga-group.nl>
2011-11-17 19:43:57 +0000

    minor, provide more specific glue functions
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/thhrule.lisp
+++ src/thhrule.lisp
@@ -22,37 +22,99 @@
 	(make-fmt-recognizer "%Y-%B-%dT%H:%M:%S")
 	(make-fmt-recognizer "%Y-%B-%dT%H:%M:%S%Z")))
 
-(defun parse-time/glue (str)
+(defun parse-time/glue-all (str)
   (let (u)
     (cond
-     ((setq u (parse-time str +datetime-recognisers+))
+     ((setq u (cybertiggyr-time::parse-time str +datetime-recognisers+))
       (make-datetime :unix u))
-     ((setq u (parse-time str +date-recognisers+))
+     ((setq u (cybertiggyr-time::parse-time str +date-recognisers+))
       (make-date :unix u))
-     ((setq u (parse-time str +time-recognisers+))
+     ((setq u (cybertiggyr-time::parse-time str +time-recognisers+))
       (make-time :unix u))
-     ((setq u (parse-time str))
+     ((setq u (cybertiggyr-time::parse-time str))
       (make-stamp :unix u)))))
+
+(defun parse-time/glue-date (str)
+  (let (u)
+    (cond
+     ((setq u (cybertiggyr-time::parse-time str +date-recognisers+))
+      (make-date :unix u)))))
+
+(defun parse-time/glue-time (str)
+  (let (u)
+    (cond
+     ((setq u (cybertiggyr-time::parse-time str +time-recognisers+))
+      (make-date :unix u)))))
+
+(defun parse-time/glue-datetime (str)
+  (let (u)
+    (cond
+     ((setq u (cybertiggyr-time::parse-time str +datetime-recognisers+))
+      (make-date :unix u)))))
 
 
 ;; rule class
+(defparameter +dawn-of-time+ (make-stamp :unix 0))
+(defparameter +dusk-of-time+ (make-stamp :unix 4294967295))
+
 (defclass rule ()
   (
    ;; validity forms first
-   (:from
-    :type stamp)
-   (:till
-    :type stamp)))
+   (from
+    :initarg :from
+    :initform +dawn-of-time+)
+   (till
+    :initarg :till
+    :initform +dusk-of-time+)
+   ;; stream closure, takes stamp and returns the next occurrence
+   (next
+    :initarg :next
+    :type function
+    :accessor next)))
 
 (defmacro make-rule (&rest stuff)
   `(make-instance 'rule ,@stuff))
 
-(defun parse-dtval (thing)
-  (cond
-   ((stringp thing)
-    (parse-time/glue thing))
-   ((member (type-of thing) '(stamp date tod datetime))
-    thing)))
+(defgeneric parse-date (thing))
+(defgeneric parse-time (thing))
+(defgeneric parse-datetime (thing))
+(defgeneric parse-dtall (thing))
+
+(defmethod parse-date ((s string))
+  (parse-time/glue-date s))
+
+(defmethod parse-date ((s symbol))
+  (parse-time/glue-date (symbol-name s)))
+
+(defmethod parse-date ((s stamp))
+  s)
+
+(defmethod parse-time ((s string))
+  (parse-time/glue-time s))
+
+(defmethod parse-time ((s symbol))
+  (parse-time/glue-time (symbol-name s)))
+
+(defmethod parse-time ((s stamp))
+  s)
+
+(defmethod parse-datetime ((s string))
+  (parse-time/glue-datetime s))
+
+(defmethod parse-datetime ((s symbol))
+  (parse-time/glue-datetime (symbol-name s)))
+
+(defmethod parse-datetime ((s stamp))
+  s)
+
+(defmethod parse-dtall ((s string))
+  (parse-time/glue-all s))
+
+(defmethod parse-dtall ((s symbol))
+  (parse-time/glue-all (symbol-name s)))
+
+(defmethod parse-dtall ((s stamp))
+  s)
 
 ;; some macros
 (defmacro defholiday (&rest ignore))
@@ -60,16 +122,20 @@
 (defmacro defholiday/yearly (&rest ignore))
 
 (defmacro defholiday/once (name &key from till on)
-  (let ((on/stamp (parse-dtval on))
-	(from/stamp (parse-dtval from))
-	(till/stamp (parse-dtval till)))
+  (let ((on/stamp (parse-date on))
+	(from/stamp (parse-dtall from))
+	(till/stamp (parse-dtall till)))
     ;; produce a closure for now
-    `(defun ,name (stamp)
-       (and
-	(or (null ,from/stamp) (d>= stamp ,from/stamp))
-	(or (null ,till/stamp) (d<= stamp ,till/stamp))
-	(d>= ,on/stamp stamp)
-	,on/stamp))))
+    `(defvar ,name
+       (make-rule
+	:from (or ,from/stamp +dawn-of-time+)
+	:till (or ,till/stamp +dusk-of-time+)
+	:next (lambda (stamp)
+		(and
+		 (or (null ,from/stamp) (d>= stamp ,from/stamp))
+		 (or (null ,till/stamp) (d<= stamp ,till/stamp))
+		 (d>= ,on/stamp stamp)
+		 ,on/stamp))))))
 
 (provide :thhrule)
 
