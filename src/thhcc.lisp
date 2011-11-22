8704afe7b990ea1973ff180402fdd6c8937aff45
Sebastian Freundt <freundt@ga-group.nl>
2011-11-22 15:12:07 +0000

    minor, provide a simple thhcc util that will become the thh compiler
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ src/thhcc.lisp
@@ -0,0 +1,38 @@
+(require "asdf")
+(asdf:load-system :thhrule)
+
+(use-package :thhrule)
+
+(defun my-command-line ()
+  (or 
+   #+clisp *args*
+   #+sbcl (cdr *posix-argv*)
+   #+cmu extensions:*command-line-words*
+   #+gcl si:*command-args*
+   nil))
+
+(defun main ()
+  ;; ./thhcc FILE RULESET
+  (let* ((cmd-line (my-command-line))
+	 (rule-file (probe-file (car cmd-line)))
+	 (ruleset/str (cadr cmd-line))
+	 ruleset/sym)
+
+    (unless rule-file
+      (error "cannot read file ~s" (car cmd-line)))
+    (load rule-file)
+
+    (unless (and (stringp ruleset/str)
+		 (nstring-upcase ruleset/str)
+		 (setq ruleset/sym (find-symbol ruleset/str)))
+      (error "ruleset ~a not defined" ruleset/str))
+    (dotimes (i 64)
+      (print (multiple-value-list (next-event (symbol-value ruleset/sym))))))
+  (quit))
+
+#+sbcl
+(sb-ext:save-lisp-and-die "thhcc.o" :executable t :toplevel #'main)
+#+clisp
+(ext:saveinitmem "thhcc.o" :executable t :norc t :quiet t :init-function #'main)
+#+cmu
+(save-lisp "thhcc.o" :executable t :load-init-file nil :init-function #'main)
