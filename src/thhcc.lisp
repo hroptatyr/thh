21e4fd8cf8524c8acfa6bb606f76ed3cbc82e1e0
Sebastian Freundt <freundt@ga-group.nl>
2011-12-01 14:26:41 +0000

    cosmetics, use exported dt<
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/thhcc.lisp
+++ src/thhcc.lisp
@@ -65,7 +65,7 @@
 	and cutoff = (or metro-end (make-stamp :unix 4294967295))
 	and d and s and r and e
 	while (and (multiple-value-setq (d s r e) (next-event rs))
-		   (thhrule::dt< (metronome-of rs) cutoff))
+		   (dt< (metronome-of rs) cutoff))
 	do (format t "~a	~a	~a	~a~%" d s r e))))
   (quit))
 
